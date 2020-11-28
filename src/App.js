7e54eb17f7c05f0455559f3c6a42bd0c834e7a8e
Mohamed Mamdouh <midomamdouh96@gmail.com>
2020-11-28 20:01:24 +0200

    5. Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+// import Glogbal Style
+import GlobalStyle from "./components/GlobalStyle";
+
 // import pages
 const { default: AboutUs } = require("./pages/AboutUs");
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,31 +1,72 @@
 // import images
 import home1 from "../img/home1.jpg";
 
+// import styled
+import styled from "styled-components";
+
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               <span>Dreams</span> come{" "}
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true .</h2>
-          </div>
+          </Hide>
         </div>
         <p>contact us for any web services</p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={home1} alt="" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+// Styled Component
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #fff;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    /* max-width: 270px; */
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+    -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+    filter: grayscale(80%);
+    border-radius: 5px;
+    flex: 1;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
