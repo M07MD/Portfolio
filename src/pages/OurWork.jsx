bc51121115af2c99b83eca29a9a956376935edd4
Mohamed Mamdouh <midomamdouh96@gmail.com>
2020-11-30 00:53:10 +0200

    10. Our Work Component

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -1,9 +1,64 @@
+// styled
+import styled from "styled-components";
+
+// Router
+import { Link } from "react-router-dom";
+
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
 const OurWork = () => {
   return (
-    <div>
-      <h1>Work </h1>
-    </div>
+    <Work>
+      <Project>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="Project" />
+        </Link>
+      </Project>
+
+      <Project>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="Project2" />
+        </Link>
+      </Project>
+
+      <Project>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="Project3" />
+        </Link>
+      </Project>
+    </Work>
   );
 };
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Project = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default OurWork;
