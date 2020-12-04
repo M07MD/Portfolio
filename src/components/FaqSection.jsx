558acb915a2b65ecd724a09cc79f8fd5e9da21fc
Mohamed Mamdouh <midomamdouh96@gmail.com>
2020-12-05 00:47:49 +0200

    FAQ Animation

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -3,59 +3,59 @@ import React from "react";
 import styled from "styled-components";
 import { About } from "../styles";
 
+// Toggle Component
+import Toggle from "./Toggle";
+
+// Framer Motion
+import { AnimateSharedLayout } from "framer-motion";
+
 const FaqSection = () => {
   return (
     <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
-            magni.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
+              magni.
+            </p>
+          </div>
+        </Toggle>
 
-      <div className="question">
-        <h4>Dialy Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
-            magni.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        <Toggle title="Dialy Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
+              magni.
+            </p>
+          </div>
+        </Toggle>
 
-      <div className="question">
-        <h4>Differrent Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
-            magni.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        <Toggle title="Differrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
+              magni.
+            </p>
+          </div>
+        </Toggle>
 
-      <div className="question">
-        <h4>What Products Do You Offer .</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
-            magni.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        <Toggle title="What Products Do You Offer .">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis,
+              magni.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const Toggle = ({ children, title }) => {
+  console.log(children);
+
+  const [toggle, setToggle] = useState(true);
+  return (
+    <motion.div layout className="question" onClick={() => setToggle(!toggle)}>
+      <motion.h4>{title}</motion.h4>
+      {toggle ? children : ""}
+      <div className="faq-line"></div>
+    </motion.div>
+  );
+};
+
+export default Toggle;
